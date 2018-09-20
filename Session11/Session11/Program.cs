1dea07a934980a685ea8b33cc28e586c03364a9e
Jakob Kallin <jakob@jakobkallin.com>
2018-09-20 15:15:06 +0200

    Session 11

---
+++ Session11/Session11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { };
+            numbers.Add(5);
+            numbers.Add(7);
+            numbers.Add(2);
+            numbers.Add(9);
+
+            List<int> greaterThanFive = new List<int> { };
+            foreach (int x in numbers)
+            {
+                if (x > 5)
+                {
+                    greaterThanFive.Add(x);
+                }
+            }
+
+            List<int> alsoGreaterThanFive = numbers.Where(x => x > 5).ToList();
+            List<int> multipliedByTwo = numbers.Select(x => x * 2).ToList();
+        }
+    }
+}
