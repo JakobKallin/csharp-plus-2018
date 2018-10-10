8bf2afe8c66865d162dbcd91d02e3336906197b8
Jakob Kallin <jakob@jakobkallin.com>
2018-10-10 22:18:08 +0200

    Session 17 cat images

--- Session17/Session17/MyForm.cs
+++ Session17/Session17/MyForm.cs
@@ -5,74 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.IO;
 
 namespace Session17
 {
-    class Film
-    {
-        public string Title;
-        public int Year;
-        public string Summary;
-    }
-
     class MyForm : Form
     {
-        private Film[] movies;
-        private Label summaryLabel;
+        private FlowLayoutPanel flow;
 
         public MyForm()
         {
-            Text = "Movie Info";
-            Size = new Size(400, 300);
-            Font = new Font("Arial", 20);
-
-            movies = new Film[]
+            flow = new FlowLayoutPanel
             {
-                new Film { Title = "Star Wars", Year = 1977, Summary = "War in space."},
-                new Film { Title = "The Matrix", Year = 1999, Summary = "Folk i svarta kappor käkar röda piller." },
-                new Film { Title = "Se7en", Year = 1995, Summary = "Brad Pitt och Morgan Freeman jagar en seriemördare." },
-                new Film { Title = "Sjunde inseglet", Year = 1957, Summary = "Jag är döden, vill du spela schack?"},
-                new Film { Title = "Man on the Moon", Year = 1999, Summary = "En man som trollar showbusinessvärlden." },
-                new Film { Title = "Superman", Year = 1978, Summary = "Is it a bird or a plane? No, it's me!"}
-            };
-
-            TableLayoutPanel table = new TableLayoutPanel
-            {
-                ColumnCount = 2,
+                FlowDirection = FlowDirection.LeftToRight,
                 Dock = DockStyle.Fill
             };
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
-            Controls.Add(table);
+            Controls.Add(flow);
 
-            foreach (Film m in movies)
+            string[] filenames = Directory.GetFiles("images");
+            foreach (string name in filenames)
             {
-                if (m.Year >= 1990)
-                {
-                    Label movieLabel = new Label
-                    {
-                        Text = m.Title + " (" + m.Year + ")",
-                        Dock = DockStyle.Fill,
-                        AutoSize = true
-                    };
-                    table.Controls.Add(movieLabel);
-                    Button movieButton = new Button
-                    {
-                        Text = "Show Summary",
-                        AutoSize = true
-                    };
-                    movieButton.Tag = m;
-                    movieButton.Click += SummaryButtonClicked;
-                    table.Controls.Add(movieButton);
-                }
+                CreatePicture(name);
             }
         }
 
-        private void SummaryButtonClicked(object sender, EventArgs e)
+        private void CreatePicture(string path)
         {
-            Button button = (Button)sender;
-            Film m = (Film)button.Tag;
-            MessageBox.Show("Summary: " + m.Summary);
+            PictureBox box1 = new PictureBox
+            {
+                Image = Image.FromFile(path),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Width = 150,
+                Height = 150
+            };
+            flow.Controls.Add(box1);
         }
     }
 }
