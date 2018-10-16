b47ad14bff8d548bb124c18fe0ecb1c2aadd4141
Jakob Kallin <jakob@jakobkallin.com>
2018-10-16 16:23:34 +0200

    Session 19 part 3

---
+++ Session19Part3/Session19Part3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session19Part3
+{
+    class Account
+    {
+        private int balance;
+        public int Balance {
+            get
+            {
+                return balance;
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (amount > balance)
+            {
+                balance -= 100;
+            }
+            balance -= amount;
+        }
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account();
+            a.Deposit(500);
+            a.Withdraw(700);
+            Console.WriteLine("Your current balance is: " + a.Balance);
+        }
+    }
+}
