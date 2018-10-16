b176659e19717d2f7d33a1a7917b8e892fc2707e
Jakob Kallin <jakob@jakobkallin.com>
2018-10-16 13:21:22 +0200

    Session 19

---
+++ Session19/Session19/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session19
+{
+    class Person
+    {
+        public string Name;
+        private int Age;
+
+        public void SetAge(int a)
+        {
+            if (a >= 0)
+            {
+                Age = a;
+            }
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+    }
+
+    class PersonWithProperties
+    {
+        public string Name;
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    age = value;
+                }
+            }
+        }
+    }
+
+    class PersonWithAutoProperties
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Person p = new Person();
+            p.Name = name;
+            p.SetAge(a);
+
+            int nextAge = p.GetAge() + 1;
+            Console.WriteLine("Next year, " + p.Name + " will be " + nextAge + " years old");
+
+            /////
+
+            PersonWithProperties p2 = new PersonWithProperties();
+            p2.Name = name;
+            p2.Age = a;
+
+            int nextAge2 = p2.Age + 1;
+            Console.WriteLine("Next year, " + p2.Name + " will be " + nextAge2 + " years old");
+            /////
+
+            PersonWithAutoProperties p3 = new PersonWithAutoProperties();
+            p3.Name = name;
+            p3.Age = a;
+
+            int nextAge3 = p3.Age + 1;
+            Console.WriteLine("Next year, " + p3.Name + " will be " + nextAge3 + " years old");
+        }
+    }
+}
---
+++ Session19Part2/Session19Part2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session19Part2
+{
+    class Building
+    {
+        private double temperature;
+        public double Temperature
+        {
+            get { return temperature; }
+            set
+            {
+                if (value >= 18 && value <= 24)
+                {
+                    temperature = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Temperature must be between 18 and 24!");
+                }
+            }
+        }
+
+        public Building(double t)
+        {
+            Temperature = t;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter temperature: ");
+            double t = double.Parse(Console.ReadLine());
+            try
+            {
+                Building house = new Building(t);
+            }
+            catch
+            {
+                Console.WriteLine("Could not create building!");
+            }
+        }
+    }
+}
