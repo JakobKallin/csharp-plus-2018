03aa7a8a50d44002cbffa95aecbaf76f467048e6
Jakob Kallin <jakob@jakobkallin.com>
2018-10-30 10:25:05 +0100

    Session 24 triangles

---
+++ Session24Part2/Session24Part2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session24Part2
+{
+    public class Triangle
+    {
+        public static bool IsIsosceles(double a, double b, double c)
+        {
+            if (a < 0 || b < 0 || c < 0)
+            {
+                return false;
+            }
+            else
+            {
+                return a == b || a == c || b == c;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Session24Part2/TriangleTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriangleTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriangleTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c323bfce-d482-4fcc-ba65-e6a94ca9799e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
